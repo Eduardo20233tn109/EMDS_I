67e89f45eaa418c317d034cdab6a685c98711c98
Eduardo <20233tn109@utez.edu.mx>
2024-11-13 08:25:31 -0600

    proyecto de spring

---
+++ Spring/SGR/src/test/java/com/example/SGR/SgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SGR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
